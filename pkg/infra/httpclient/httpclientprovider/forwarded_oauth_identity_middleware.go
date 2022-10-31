ddf0dd8a80238237ed8f2cd80c01a97cfbfceb49
lucostus <constantin.winkler@gepardec.com>
2022-10-31 08:33:59 +0100

    GPX-495: Some middleware

--- pkg/infra/httpclient/httpclientprovider/forwarded_oauth_identity_middleware.go
+++ pkg/infra/httpclient/httpclientprovider/forwarded_oauth_identity_middleware.go
@@ -1,8 +1,10 @@
 package httpclientprovider
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
 	"golang.org/x/oauth2"
@@ -18,7 +20,29 @@ func ForwardedOAuthIdentityMiddleware(token *oauth2.Token) httpclient.Middleware
 			return next
 		}
 		return httpclient.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
-			req.Header.Set("Authorization", fmt.Sprintf("%s %s", token.Type(), token.AccessToken))
+			// TODO: take client secret client id from configuration. add additional property for audience, requested issuer and url. add additional middleware controller for token exchange and
+			data := url.Values{
+				"client_id":        {"grafana"},
+				"client_secret":    {"replace me"},
+				"subject_token":    {token.AccessToken},
+				"requested_issuer": {"openshift"},
+				"audience":         {"grafana"},
+				"grant_type":       {"urn:ietf:params:oauth:grant-type:token-exchange"},
+			}
+
+			resp, err := http.PostForm("replace me", data)
+
+			if err != nil {
+				return next.RoundTrip(req)
+			}
+
+			var res map[string]interface{}
+
+			json.NewDecoder(resp.Body).Decode(&res)
+
+			fmt.Println(res["form"])
+
+			req.Header.Set("Authorization", fmt.Sprintf("%s", res["access_token"]))
 
 			idToken, ok := token.Extra("id_token").(string)
 			if ok && idToken != "" {
